f44a27839a7134f180f61de26b9e32a420aa686b
Juan Manuel Torres <juan.manuel.torres@mi.unc.edu.ar>
2026-08-29 17:05:00 -0300

    style: switch fleet map to dark basemap

--- src/map/mapConfig.ts
+++ src/map/mapConfig.ts
@@ -1,6 +1,6 @@
 export const MAP_CENTER: [number, number] = [-64.1888, -31.4201]
 export const MAP_ZOOM = 12.4
-export const MAP_STYLE = 'https://tiles.openfreemap.org/styles/liberty'
+export const MAP_STYLE = 'https://tiles.openfreemap.org/styles/dark'
 
 export const SOURCE_ROUTES = 'fleet-routes'
 export const SOURCE_STORES = 'fleet-stores'
