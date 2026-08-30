90020cb94d6aa3ac331a3b56923c13134f82ae82
Juan Manuel Torres <juan.manuel.torres@mi.unc.edu.ar>
2026-08-30 12:12:29 -0300

    style: load V0.5 UI polish

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import App from './App'
 import './app.css'
+import './ui-polish.css'
 import { configureMapLibreWorker } from './map/mapWorker'
 
 configureMapLibreWorker()
